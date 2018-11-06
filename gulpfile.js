ed850d8c3428634e29c869f37b4fc3f8d1c61a37
Andrey Miskov <amiskov@oggettoweb.com>
2018-11-06 09:46:34 +0300

    Update packages and build settings

--- gulpfile.js
+++ gulpfile.js
@@ -20,34 +20,34 @@ gulp.task('livereload', () => {
 });
 
 gulp.task('styles', () => {
-    gulp.src('src/less/main.less')
+    return gulp.src('src/less/main.less')
         .pipe(less())
         .pipe(autoprefixer())
         .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('img', () => {
-    gulp.src('src/img/**/*.*')
+    return gulp.src('src/img/**/*.*')
         .pipe(gulp.dest('./dist/img'));
 });
 
 gulp.task('js', () => {
-    gulp.src('src/js/**/*.*')
+    return gulp.src('src/js/**/*.*')
         .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('html', () => {
-    gulp.src('src/index.html')
+    return gulp.src('src/index.html')
         .pipe(gulp.dest('./dist'));
 });
 
 // Отслеживание изменений в файлах, нужно только при локальной разработке
 gulp.task('watch', () => {
-    gulp.watch('src/less/**/*.less', ['styles']);
-    gulp.watch('src/**/*.html', ['html']);
-    gulp.watch('src/img/**/*.*', ['img']);
-    gulp.watch('src/js/**/*.*', ['js']);
+    gulp.watch('src/less/**/*.less', gulp.parallel('styles'));
+    gulp.watch('src/**/*.html', gulp.parallel('html'));
+    gulp.watch('src/img/**/*.*', gulp.parallel('img'));
+    gulp.watch('src/js/**/*.*', gulp.parallel('js'));
 });
 
-gulp.task('default', ['styles', 'html', 'img', 'js', 'livereload', 'watch']);
-gulp.task('prod', ['styles', 'html', 'img', 'js']);
+gulp.task('default', gulp.parallel('styles', 'html', 'img', 'js', 'livereload', 'watch'));
+gulp.task('prod', gulp.parallel('styles', 'html', 'img', 'js'));
