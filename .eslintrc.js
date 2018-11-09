894809bb062b24d971c0e6b79d1dd5832c219cc2
UnShame <mrunshame@gmail.com>
2018-11-09 09:45:02 +0300

    First css pass, add selector functionality

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,9 @@ module.exports = {
         'semi': [
             'error',
             'always'
+        ],
+        'no-unused-vars': [
+            'warn'
         ]
     },
     'parserOptions': {
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,24 @@
-// Your code here...
+function createSelectorControl(selector, onchange, initialIndex = 0) {
+
+    const styleCurrent = 'selector__button_current';
+
+    let $elem = $(selector);
+    let $buttons = $('.selector__button', $elem);
+    let $curButton = $($buttons[initialIndex]);
+    $curButton.addClass(styleCurrent);
+    onchange($curButton[0]);
+
+    $buttons.click((e) => {
+        let button = e.target;
+        $curButton.removeClass(styleCurrent);
+        $curButton = $(button);
+        $curButton.addClass(styleCurrent);
+        onchange(button);
+    });
+}
+
+let $preview = $('.item__preview');
+createSelectorControl('.item__size-selector', () => {});
+createSelectorControl('.item__color-selector', (button) => {
+    $preview.attr('src', './img/tee-' + button.dataset.color + '.png');
+});
