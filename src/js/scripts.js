04486eae61e5e2554751658483e5db0c4ac48c22
UnShame <mrunshame@gmail.com>
2018-11-11 20:26:18 +0300

    Rewrite scrips without jquery as per the assignment

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,27 +2,30 @@ function createSelectorControl(selector, onchange, initialIndex = 0) {
 
     const styleCurrent = 'selector__button_current';
 
-    let $elem = $(selector);
-    let $buttons = $('.selector__button', $elem);
-    let $curButton = $($buttons[initialIndex]);
-    $curButton.addClass(styleCurrent);
-    onchange($curButton[0]);
+    let elem = document.querySelector(selector);
+    let buttons = elem.querySelectorAll('.selector__button');
+    let curButton = buttons[initialIndex];
+    curButton.classList.add(styleCurrent);
+    onchange(curButton);
 
-    $buttons.click((e) => {
-        select(e.target);
-    });
+    for(let button of buttons) {
+        button.addEventListener('click', () => {
+            select(button);
+        });
+    }
+
 
     function select(button) {
-        $curButton.removeClass(styleCurrent);
-        $curButton = $(button);
-        $curButton.addClass(styleCurrent);
+        curButton.classList.remove(styleCurrent);
+        curButton = button;
+        curButton.classList.add(styleCurrent);
         onchange(button);
     }
 
     return {
-        elem: $elem,
+        elem: elem,
         select(i) {
-            select($buttons[i]);
+            select(buttons[i]);
         }
     };
 }
@@ -30,16 +33,6 @@ function createSelectorControl(selector, onchange, initialIndex = 0) {
 function getImageUrl(color) {
     return './img/tee-' + color + '.png';
 }
-
-function preloadColors() {
-    let $buttons = $('.item__color-selector .selector__button');
-    $buttons.each((i, button) => {
-        let img = new Image();
-        img.src = getImageUrl(button.dataset.color);
-    });
-}
-
-preloadColors();
 
 let $preview = $('.item__preview');
 createSelectorControl('.item__size-selector', () => {});
