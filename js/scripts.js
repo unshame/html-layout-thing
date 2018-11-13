0f020ab94da3d5cb17544b0daaecc8ea31aeed2e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-13 19:15:59 +0000

    Deploy unshame/html-layout-thing to github.com/unshame/html-layout-thing.git:gh-pages

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+function createSelectorControl(selector, onchange, initialIndex = 0) {
+
+    const styleCurrent = 'selector__button_current';
+
+    let elem = document.querySelector(selector);
+    let buttons = elem.querySelectorAll('.selector__button');
+    let curButton = buttons[initialIndex];
+    curButton.classList.add(styleCurrent);
+    onchange(curButton);
+
+    for(let button of buttons) {
+        button.addEventListener('click', () => {
+            select(button);
+        });
+    }
+
+
+    function select(button) {
+        curButton.classList.remove(styleCurrent);
+        curButton = button;
+        curButton.classList.add(styleCurrent);
+        onchange(button);
+    }
+
+    return {
+        elem: elem,
+        select(i) {
+            select(buttons[i]);
+        }
+    };
+}
+
+function getImageUrl(color) {
+    return './img/tee-' + color + '.png';
+}
+
+let preview = document.querySelector('.item__preview');
+createSelectorControl('.item__size-selector', () => {});
+createSelectorControl('.item__color-selector', (button) => {
+    preview.src = getImageUrl(button.dataset.color);
+});
